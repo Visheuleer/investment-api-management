5ce04d95f65f92321c2b3a591895024e6711b5ab
BANCOARBI\jonas.queiroz <jonas.queiroz@bancoarbi.com.br>
2024-11-22 16:28:14 -0300

    Create owner model

---
+++ src/models/owner_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, CHAR, String
+from models import Base
+import uuid
+
+
+class Owner(Base):
+    __tablename__ = "owners"
+
+    id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid.uuid4()), index=True)
+    fullname = Column(String(255), nullable=False, index=True)
+    document = Column(String(11), nullable=False, unique=True, index=True)
