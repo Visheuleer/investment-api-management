0bb0d8365d53b975f14eb2934023fbfb1438e2ac
BANCOARBI\jonas.queiroz <jonas.queiroz@bancoarbi.com.br>
2024-11-22 17:03:16 -0300

    Refactor OwnerRepository to use static methods

--- src/repositories/owner_repository.py
+++ src/repositories/owner_repository.py
@@ -2,22 +2,22 @@ from db import db_connection
 from models import OwnerModel
 
 class OwnerRepository:
-    def __init__(self, owner: dict):
-        self.owner = owner
+    def __init__(self):
+        pass
 
-
-    def find_owner_by_document(self) -> OwnerModel:
+    @staticmethod
+    def find_owner_by_document(document:str) -> OwnerModel:
         session = db_connection.db_session()
         owner = session.query(OwnerModel).filter(
-            OwnerModel.document == self.owner['document']
+            OwnerModel.document == document
         ).first()
         session.close()
         return owner
 
-
-    def save_owner(self):
+    @staticmethod
+    def save_owner(owner:dict):
         session = db_connection.db_session()
-        session.add(OwnerModel(**self.owner))
+        session.add(OwnerModel(**owner))
         session.commit()
         session.close()
 
