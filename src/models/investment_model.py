9a53cfc1d11695040c56e896dbc67899c0b7dec3
Jonas <jonaseuler98@gmail.com>
2024-11-28 14:34:30 -0300

    status investment model

--- src/models/investment_model.py
+++ src/models/investment_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, CHAR, String, Float, DateTime, ForeignKey
+from sqlalchemy import Column, CHAR, String, Float, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from models import Base
 import datetime
@@ -12,6 +12,7 @@ class InvestmentModel(Base):
     owner_id = Column(CHAR(36), ForeignKey("owners.id"), nullable=False, index=True)
     name = Column(String(255), nullable=False, index=True)
     amount_invested = Column(Float, nullable=False, index=True)
+    status = Column(Integer, nullable=False, index=True)
     created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
 
     owner = relationship("OwnerModel", back_populates="investment")
